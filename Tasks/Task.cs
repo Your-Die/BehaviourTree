21335b783845a3aa03b6ca3db076e5e62e99eef3
Jordy <jordy_van_opstal@live.nl>
2018-02-11 14:41:54 +0100

    Implement Task, a base class for monobehaviour behaviours.

---
+++ Tasks/Task.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+using Chinchillada.BehaviourSelections.BehaviourTree.Builder;
+using Status = Chinchillada.BehaviourSelections.BehaviourTree.Behaviour.Status;
+
+namespace Chinchillada.BehaviourSelections.BehaviourTree.Tasks
+{
+    /// <summary>
+    /// Base class for defining <see cref="BehaviourTree"/> behaviours as <see cref="MonoBehaviour"/>.
+    /// </summary>
+    internal abstract class Task : MonoBehaviour, IBehaviourBuilder
+    {
+        /// <summary>
+        /// Updates every frame while this <see cref="Task"/> is active.
+        /// </summary>
+        /// <returns>The status after this update.</returns>
+        protected abstract Status UpdateInternal();
+
+        /// <inheritdoc />
+        public IBehaviour Build(BehaviourTree tree)
+        {
+            return new TaskBehaviour(UpdateInternal, tree);
+        }
+
+        /// <summary>
+        /// <see cref="Behaviour"/> that the <see cref="Task"/> wraps.
+        /// </summary>
+        private class TaskBehaviour : Behaviour
+        {
+            private readonly Func<Status> _updateDelegate;
+
+            public TaskBehaviour(Func<Status> updateDelegate, BehaviourTree tree) : base(tree)
+            {
+                _updateDelegate = updateDelegate;
+            }
+
+            protected override Status UpdateInternal()
+            {
+                return _updateDelegate();
+            }
+        }
+    }
+}
