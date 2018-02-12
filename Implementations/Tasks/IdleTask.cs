b941d1b6d44f209eac8fc3432919ad6033952ce3
Jordy <jordy_van_opstal@live.nl>
2018-02-12 15:31:25 +0100

    Implement idle task.

---
+++ Implementations/Tasks/IdleTask.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using Status = Chinchillada.BehaviourSelections.BehaviourTree.Behaviour.Status;
+
+namespace Chinchillada.BehaviourSelections.BehaviourTree.Tasks
+{
+    internal class IdleTask : Task
+    {
+        [SerializeField] private float _duration; 
+
+        protected override void OnInitialization()
+        {
+            Behaviour.Suspend();
+            Invoke(nameof(Finish), _duration);
+        }
+
+        protected override Status UpdateInternal()
+        {
+            return Behaviour.Status;
+        }
+
+        private void Finish()
+        {
+            Behaviour.Terminate(Status.Succes);
+        }
+    }
+}
