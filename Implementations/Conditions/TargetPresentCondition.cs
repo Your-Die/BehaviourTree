c2f0ea8b65b743755e5ff279fa43de7dba1cd843
Jordy <jordy_van_opstal@live.nl>
2018-02-12 15:26:42 +0100

    Implement some conditions.

---
+++ Implementations/Conditions/TargetPresentCondition.cs
@@ -0,0 +1,21 @@
+﻿using Assets.BehaviourSelections.BahaviorTree.Builder;
+using Chinchillada.BehaviourSelections.BehaviourTree.Tasks;
+using UnityEngine;
+
+namespace Chinchillada.BehaviourSelections.BehaviourTree
+{
+    internal class TargetPresentCondition : ConditionBuilder
+    {
+        [SerializeField] private ITargeter _targeter;
+
+        private void Awake()
+        {
+            _targeter = GetComponentInParent<ITargeter>();
+        }
+
+        protected override bool ValidateCondition()
+        {
+            return _targeter.GetTarget() != null;
+        }
+    }
+}
