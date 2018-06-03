3d8cd250c2c545f162d9afc96afc23d2e3b7d3a8
Jordy <jordy_van_opstal@live.nl>
2018-06-03 22:43:56 +0200

    Add circle move task.
    Task that moves character in a circle around the target.

---
+++ Implementations/Tasks/Movement/CircleMoveTask.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using Status = Chinchillada.BehaviourSelections.BehaviourTree.Behaviour.Status;
+
+namespace Chinchillada.BehaviourSelections.BehaviourTree.Tasks
+{ 
+    public class CircleMoveTask : MovementTask
+    {
+        private float _angle;
+
+        protected override Status UpdateInternal()
+        {
+            //Ensure target.
+            if (!Targeter.HasTarget())
+                return Status.Failure;
+
+            //Update angle.
+            _angle += MovementController.SpeedPerUpdate;
+
+            //Calculate offset.
+            float sin = Mathf.Sin(_angle);
+            float cos = Mathf.Cos(_angle);
+            Vector3 offset = new Vector2(sin, cos);
+
+            //Calculate new position.
+            offset *= Targeter.DistanceToTarget();
+            Vector2 position = Target.position + offset;
+
+            //Move towards new position.
+            MovementController.MoveTowards(position);
+            return Status.Running;
+        }
+    }
+}
