e9fc5d084eabf9df5934b99b4048f83565ebaf64
Jordy <jordy_van_opstal@live.nl>
2018-06-05 13:36:11 +0200

    Implement ActiveSelector.

---
+++ Framework/Behaviours/Composites/ActiveSelector.cs
@@ -0,0 +1,45 @@
+﻿namespace Chinchillada.BehaviourSelections.BehaviourTree
+{
+    /// <summary>
+    /// Selector that validates the selection each update.
+    /// </summary>
+    public class ActiveSelector : Selector
+    {
+        /// <summary>
+        /// Index of the child that was selected the previous update.
+        /// </summary>
+        private int _previousChildIndex;
+
+        public ActiveSelector(BehaviourTree tree) : base(tree)
+        {
+        }
+
+        protected override void Initialize()
+        {
+            _previousChildIndex = -1;
+            Start();
+        }
+
+        /// <summary>
+        /// Re-initialize the base Selector so it revaluates the selection.
+        /// </summary>
+        private void Start()
+        {
+            base.Initialize();
+            Continue();
+        }
+
+        protected override Status UpdateInternal()
+        {
+            //Check if we reached the previously active child. If not, cancel it.
+            if(CurrentChildIndex < _previousChildIndex)
+                Children[_previousChildIndex].Terminate(Status.Failure);
+
+            //Save currently active child, and evaluate.
+            _previousChildIndex = CurrentChildIndex;
+            Start();
+
+            return Status.Running;
+        }
+    }
+}
