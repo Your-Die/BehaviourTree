4c58f5fdb3ead6c45cb942ee3fc4abc752b01147
Jordy <jordy_van_opstal@live.nl>
2018-06-24 14:14:52 +0200

    Implement UnityActionBuilder.
    Action builder that delegates the action to a unity event element.

---
+++ Framework/Components/BehaviourBuilders/UnityActionBuilder.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine.Events;
+
+namespace Chinchillada.BehaviourSelections.BehaviourTree.Builder
+{
+    internal class UnityActionBuilder : ActionBuilder
+    {
+        public UnityEvent Action;
+
+        protected override bool DoAction()
+        {
+            Action.Invoke();
+            return true;
+        }
+    }
+}
