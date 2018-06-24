b09b178d06646d520acdca3492db3902b9c640c3
Jordy <jordy_van_opstal@live.nl>
2018-06-24 12:24:16 +0200

    Reimplement CircleMoveTask
    It wasn't working before.

--- Implementations/Tasks/Movement/CircleMoveTask.cs
+++ Implementations/Tasks/Movement/CircleMoveTask.cs
@@ -1,39 +1,25 @@
 ﻿using UnityEngine;
+using Chinchillada.BehaviourSelections.Utilities;
 using Status = Chinchillada.BehaviourSelections.BehaviourTree.Behaviour.Status;
 
 namespace Chinchillada.BehaviourSelections.BehaviourTree.Tasks
 { 
     public class CircleMoveTask : MovementTask
     {
-        private float _angle;
-
-        protected override void OnInitialization()
-        {
-            base.OnInitialization();
-            
-            _angle = Vector2.Angle(Target.position, transform.position);
-        }
-
+        public bool Clockwise;
+      
         protected override Status UpdateInternal()
         {
             //Ensure target.
-            if (!Targeter.HasTarget())
+            if (!Targeter.HasTarget)
                 return Status.Failure;
 
-            //Update angle.
-            _angle += MovementController.SpeedPerUpdate;
-
-            //Calculate offset.
-            float sin = Mathf.Sin(_angle);
-            float cos = Mathf.Cos(_angle);
-            Vector3 offset = new Vector2(sin, cos);
-
-            //Calculate new position.
-            offset *= Targeter.DistanceToTarget();
-            Vector2 position = Target.position + offset;
-
-            //Move towards new position.
-            MovementController.MoveTowards(position);
+            Vector2 directionToTarget = Targeter.DirectionToTarget();
+            Vector2 perpendicular = Clockwise
+                ? directionToTarget.PerpendicularCW()
+                : directionToTarget.PerpendicularCCW();
+            
+            MovementController.ApplyMovement(perpendicular);
             return Status.Running;
         }
     }
---
+++ Utilities/MathHelper.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Chinchillada.BehaviourSelections.Utilities
+{
+    public static class MathHelper
+    {
+        public static float Square(this float value)
+        {
+            return Mathf.Pow(value, 2);
+        }
+
+        /// <summary>
+        /// Calculates the 2D perpendicular in clockwise direction of <paramref name="vector"/>.
+        /// </summary> 
+        public static Vector2 PerpendicularCW(this Vector2 vector)
+        {
+            float x2 = vector.x.Square();
+            float y2 = vector.y.Square();
+
+            float pythagoras = Mathf.Sqrt(x2 + y2);
+
+            return new Vector2(-vector.y, vector.x) / pythagoras;
+        }
+
+
+        /// <summary>
+        /// Calculates the 2D perpendicular in counter-clockwise direction of <paramref name="vector"/>.
+        /// </summary> 
+        public static Vector2 PerpendicularCCW(this Vector2 vector)
+        {
+            return -PerpendicularCW(vector);
+        }
+    }
+}
